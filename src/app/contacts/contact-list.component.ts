b11313312503d22eef0113ff8b8893b2a408f7a2
AngularIHM <younesb21@gmail.com>
2025-01-01 17:07:38 +0100

    Adding contact features

--- src/app/contacts/contact-list.component.ts
+++ src/app/contacts/contact-list.component.ts
@@ -1,19 +1,83 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 import { FormsModule, NgForm } from "@angular/forms";
-import { MessageModule } from "primeng/message";
 import { ButtonModule } from "primeng/button";
+import { InputTextModule } from "primeng/inputtext";
+import { InputTextareaModule } from "primeng/inputtextarea";
+
+import { NgIf } from "@angular/common";
+import { ContactService } from "./data-access/contact.service";
+import { Contact } from "./data-access/contact.model";
+
+import { MessageService } from "primeng/api";
+import { ToastModule } from "primeng/toast";
 
 @Component({
   selector: "app-contact-list",
   templateUrl: "./contact-list.component.html",
   styleUrls: ["./contact-list.component.scss"],
   standalone: true,
-  imports: [FormsModule, MessageModule, ButtonModule],
+  imports: [
+    FormsModule,
+    ButtonModule,
+    NgIf,
+    InputTextModule,
+    InputTextareaModule,
+    ToastModule,
+  ],
+  providers: [MessageService],
 })
 export class ContactForm implements OnInit {
-  ngOnInit(): void {}
+  @ViewChild('messageValue') messageValue!: ElementRef;
+  @ViewChild("contactForm") contactForm!: NgForm;
+  email: string = "";
+  message: string = "";
+  imputedSize: number = 0;
+  maxLength: number = 299;
+  private messageService = inject(MessageService);
 
-  onFormSubmit(contactForm: NgForm): void {
-    console.log(contactForm.valid);
+  ngOnInit() {}
+
+  constructor(private contactService: ContactService) {}
+
+  ngAfterViewInit() {
+    console.log(this.messageValue.nativeElement.value.length);
+  }
+
+  onTextareaInput(): void {
+    this.imputedSize = this.messageValue.nativeElement.value.length;
+  }
+
+  onFormSubmit(): void {
+    this.contactForm.control.markAllAsTouched();
+    if (this.contactForm.valid) {
+      let contact: Contact = {
+        id: crypto.randomUUID(),
+        email: this.email,
+        message: this.message,
+        date: new Date(),
+      };
+
+      this.contactService.onSendMessage(contact).subscribe((isSubmitted) => {
+        if (isSubmitted) {
+          this.messageService.add({
+            life: 5000,
+            severity: "success",
+            detail: "Demande de contact envoyée avec succès",
+          });
+          this.resetForm();
+        }
+      });
+    }
+  }
+
+  private resetForm(): void {
+    this.contactForm.resetForm();
+    this.imputedSize = 0;
   }
 }
